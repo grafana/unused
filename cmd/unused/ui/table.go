15820cf65bb8f901b4be9bc0dc5d8b9ef15c69a9
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-04-13 11:28:06 -0300

    Add verbose mode to ui.Table
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- cmd/unused/ui/table.go
+++ cmd/unused/ui/table.go
@@ -12,19 +12,30 @@ import (
 var _ Displayer = Table{}
 
 type Table struct {
-	out io.Writer
+	out     io.Writer
+	verbose bool
 }
 
-func NewTable(out io.Writer) Table {
-	return Table{out}
+func NewTable(out io.Writer, verbose bool) Table {
+	return Table{out, verbose}
 }
 
 func (t Table) Display(ctx context.Context, disks unused.Disks) error {
-	w := tabwriter.NewWriter(t.out, 8, 2, 0, ' ', 0)
+	w := tabwriter.NewWriter(t.out, 8, 4, 2, ' ', 0)
 
-	fmt.Fprintln(w, "PROVIDER\tNAME\tMETADATA")
+	fmt.Fprintln(w, "PROVIDER\tNAME")
+	if t.verbose {
+		fmt.Fprint(w, "\tMETADATA")
+	}
+	fmt.Fprintln(w)
+
 	for _, d := range disks {
-		fmt.Fprintf(w, "%s\t%s\t%s\n", d.Provider().Name(), d.Name(), d.Meta())
+		p := d.Provider()
+		if t.verbose {
+			fmt.Fprintf(w, "%s{%s}\t%s\t%s\n", p.Name(), p.Meta(), d.Name(), d.Meta())
+		} else {
+			fmt.Fprintf(w, "%s\t%s\n", p.Name(), d.Name())
+		}
 	}
 
 	if err := w.Flush(); err != nil {
