04e9cfbc245fea3e052f571c7330fb4bf8fd81b5
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-04-19 11:56:01 -0300

    Add interactive mode flag to enable interactive UI
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- cmd/unused/main.go
+++ cmd/unused/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/grafana/unused/cli"
 	"github.com/grafana/unused/cmd/unused/ui"
+	"github.com/grafana/unused/cmd/unused/ui/interactive"
 	"github.com/grafana/unused/cmd/unused/ui/table"
 	// "github.com/mmcloughlin/profile"
 )
@@ -17,17 +18,26 @@ func main() {
 	// defer profile.Start(profile.CPUProfile, profile.MemProfile).Stop()
 
 	var gcpProjects, awsProfiles, azureSubs cli.StringSliceFlag
-
 	flag.Var(&gcpProjects, "gcp.project", "GCP project ID (can be specified multiple times)")
 	flag.Var(&awsProfiles, "aws.profile", "AWS profile (can be specified multiple times)")
 	flag.Var(&azureSubs, "azure.sub", "Azure subscription (can be specified multiple times)")
+
+	var interactiveMode, verbose bool
+
+	flag.BoolVar(&interactiveMode, "i", false, "Interactive UI mode")
+	flag.BoolVar(&verbose, "v", false, "Verbose mode")
+
 	flag.Parse()
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
 	var out ui.UI
-	out = table.New(os.Stdout, false)
+	if interactiveMode {
+		out = interactive.New(verbose)
+	} else {
+		out = table.New(os.Stdout, verbose)
+	}
 
 	if err := realMain(ctx, out, gcpProjects, awsProfiles, azureSubs); err != nil {
 		cancel() // cleanup resources
