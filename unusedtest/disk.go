572d2d74c7b458a019301baa279b176ec00c4dc9
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-05-19 10:38:35 -0300

    Export unused package types
    For this package they might not make too much sense yet, but it will
    be coherent with the rest of the providers.
    
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- unusedtest/disk.go
+++ unusedtest/disk.go
@@ -6,22 +6,22 @@ import (
 	"github.com/grafana/unused"
 )
 
-var _ unused.Disk = disk{}
+var _ unused.Disk = Disk{}
 
-type disk struct {
+type Disk struct {
 	id, name  string
 	provider  unused.Provider
 	createdAt time.Time
 	meta      unused.Meta
 }
 
-func NewDisk(name string, provider unused.Provider, createdAt time.Time) disk {
-	return disk{name, name, provider, createdAt, nil}
+func NewDisk(name string, provider unused.Provider, createdAt time.Time) Disk {
+	return Disk{name, name, provider, createdAt, nil}
 }
 
-func (d disk) ID() string                { return d.name }
-func (d disk) Provider() unused.Provider { return d.provider }
-func (d disk) Name() string              { return d.name }
-func (d disk) CreatedAt() time.Time      { return d.createdAt }
-func (d disk) Meta() unused.Meta         { return d.meta }
-func (d disk) LastUsedAt() time.Time     { return d.createdAt.Add(1 * time.Minute) }
+func (d Disk) ID() string                { return d.name }
+func (d Disk) Provider() unused.Provider { return d.provider }
+func (d Disk) Name() string              { return d.name }
+func (d Disk) CreatedAt() time.Time      { return d.createdAt }
+func (d Disk) Meta() unused.Meta         { return d.meta }
+func (d Disk) LastUsedAt() time.Time     { return d.createdAt.Add(1 * time.Minute) }
