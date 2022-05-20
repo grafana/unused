20d8f8aef772d0672ee75487f5037c9759bfddb4
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-05-20 14:31:47 -0300

    Add ui.Options type
    This type will hold all the information required to display unused
    disks by the different UI implementations.
    
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- cmd/unused/ui/ui.go
+++ cmd/unused/ui/ui.go
@@ -6,6 +6,16 @@ import (
 	"github.com/grafana/unused"
 )
 
+type Filter struct {
+	Key, Value string
+}
+
+type Options struct {
+	Providers    []unused.Provider
+	ExtraColumns []string
+	Filter       Filter
+}
+
 type UI interface {
 	Display(ctx context.Context, disks unused.Disks, extraColumns []string) error
 }
