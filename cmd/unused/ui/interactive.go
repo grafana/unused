461104e3e2f7e741913bb730c079b406891022a3
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-05-24 11:50:38 -0300

    Remove old interactive UI code
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- cmd/unused/ui/interactive.go
+++ cmd/unused/ui/interactive.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/grafana/unused"
 	"github.com/grafana/unused/cmd/unused/ui/interactive"
 )
 
@@ -16,34 +15,5 @@ func Interactive(ctx context.Context, options Options) error {
 		return fmt.Errorf("cannot start interactive UI: %w", err)
 	}
 
-	return nil
-}
-
-func oldInteractive(ctx context.Context, options Options) error {
-	disks, err := listUnusedDisks(ctx, options.Providers)
-	if err != nil {
-		return err
-	}
-
-	if options.Filter.Key != "" {
-		filtered := make(unused.Disks, 0, len(disks))
-		for _, d := range disks {
-			if d.Meta().Matches(options.Filter.Key, options.Filter.Value) {
-				filtered = append(filtered, d)
-			}
-		}
-		disks = filtered
-	}
-
-	if len(disks) == 0 {
-		fmt.Println("No disks found")
-		return nil
-	}
-
-	m := interactive.NewModel(options.Verbose, disks, options.ExtraColumns)
-
-	if err := tea.NewProgram(m).Start(); err != nil {
-		return fmt.Errorf("cannot start interactive UI: %w", err)
-	}
 	return nil
 }
