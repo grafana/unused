993a7d7769c632c9c7471d4aa0962c28c4f9b176
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-05-24 15:14:56 -0300

    Add missing clicommon.Age tests
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- cmd/clicommon/age_test.go
+++ cmd/clicommon/age_test.go
@@ -15,11 +15,13 @@ func TestAge(t *testing.T) {
 		exp string
 	}{
 		{now.Add(-30 * time.Second), "1m"},
+		{now.Add(-30 * time.Minute), "30m"},
 		{now.Add(-5 * time.Hour), "5h"},
 		{now.Add(-7 * 24 * time.Hour), "7d"},
 		{now.Add(-364 * 24 * time.Hour), "364d"},
 		{now.Add(-600 * 24 * time.Hour), "1y"},
 		{now.Add(-740 * 24 * time.Hour), "2y"},
+		{time.Time{}, "n/a"},
 	}
 
 	for _, tt := range tests {
