8f86c1fe08c25020d4d81165f19ef13b44d654cd
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-05-24 16:34:21 -0300

    Retrieve AWS last used date for volume from CloudTrail events
    This is a bit of a hack and it only works on one volume at a time, yet
    it is a valid solution nonetheless. Note that it seems LookupEvents is
    only taking the first lookup attribute for filtering, so we need to
    iterate over the results later.
    
    Also, AWS only report events for the past 90 days, anything greater
    than that it will be discarded.
    
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- aws/disk.go
+++ aws/disk.go
@@ -1,8 +1,13 @@
 package aws
 
 import (
+	"context"
+	"fmt"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudtrail"
+	ctypes "github.com/aws/aws-sdk-go-v2/service/cloudtrail/types"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/grafana/unused"
 )
@@ -34,3 +39,33 @@ func (d *Disk) CreatedAt() time.Time { return *d.Volume.CreateTime }
 func (d *Disk) Meta() unused.Meta { return d.meta }
 
 func (d *Disk) LastUsedAt() time.Time { return d.lastUsed }
+
+func (d *Disk) RefreshLastUsedAt(ctx context.Context) error {
+	c := cloudtrail.NewFromConfig(d.provider.cfg)
+
+	res, err := c.LookupEvents(ctx, &cloudtrail.LookupEventsInput{
+		StartTime: d.Volume.CreateTime,
+		LookupAttributes: []ctypes.LookupAttribute{
+			{
+				AttributeKey:   ctypes.LookupAttributeKeyResourceName,
+				AttributeValue: d.Volume.VolumeId,
+			},
+			{
+				AttributeKey:   ctypes.LookupAttributeKeyEventName,
+				AttributeValue: aws.String("DetachVolume"),
+			},
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("cannot lookup DetachVolume events for Volume %s: %v", *d.VolumeId, err)
+	}
+
+	for _, e := range res.Events {
+		if *e.EventName == "DetachVolume" {
+			d.lastUsed = *e.EventTime
+			break
+		}
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/Azure/go-autorest/autorest v0.11.25
 	github.com/Azure/go-autorest/autorest/azure/auth v0.5.11
 	github.com/Azure/go-autorest/autorest/date v0.3.0
-	github.com/aws/aws-sdk-go-v2 v1.16.2
+	github.com/aws/aws-sdk-go-v2 v1.16.4
 	github.com/aws/aws-sdk-go-v2/config v1.15.3
 	github.com/aws/aws-sdk-go-v2/credentials v1.11.2
 	github.com/aws/aws-sdk-go-v2/service/ec2 v1.34.0
@@ -29,9 +29,10 @@ require (
 	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.3 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.9 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.5 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.10 // indirect
+	github.com/aws/aws-sdk-go-v2/service/cloudtrail v1.16.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.11.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.16.3 // indirect
