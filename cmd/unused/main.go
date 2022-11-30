5ffacb021f3ecf3db098e0255445858e7491fbbd
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-11-30 10:12:30 -0300

    Refactor unused flags declaration
    This simplifies code making it less indirect. It also allows us to
    remove some types that aren't really needed.
    
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- cmd/unused/main.go
+++ cmd/unused/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/grafana/unused/cmd/internal"
 	"github.com/grafana/unused/cmd/unused/ui"
@@ -18,21 +20,36 @@ func main() {
 	var (
 		gcpProjects, awsProfiles, azureSubs internal.StringSliceFlag
 
-		interactiveMode, verbose bool
+		interactiveMode bool
 
-		filter internal.FilterFlag
-
-		extraColumns internal.StringSliceFlag
+		options ui.Options
 	)
 
 	internal.ProviderFlags(flag.CommandLine, &gcpProjects, &awsProfiles, &azureSubs)
 
 	flag.BoolVar(&interactiveMode, "i", false, "Interactive UI mode")
-	flag.BoolVar(&verbose, "v", false, "Verbose mode")
+	flag.BoolVar(&options.Verbose, "v", false, "Verbose mode")
 
-	flag.Var(&filter, "filter", "Filter by disk metadata")
+	flag.Func("filter", "Filter by disk metadata", func(v string) error {
+		ps := strings.SplitN(v, "=", 2)
 
-	flag.Var(&extraColumns, "add-column", "Display additional column with metadata")
+		if len(ps) == 0 || ps[0] == "" {
+			return errors.New("invalid filter format")
+		}
+
+		options.Filter.Key = ps[0]
+
+		if len(ps) == 2 {
+			options.Filter.Value = ps[1]
+		}
+
+		return nil
+	})
+
+	flag.Func("add-column", "Display additional column with metadata", func(c string) error {
+		options.ExtraColumns = append(options.ExtraColumns, c)
+		return nil
+	})
 
 	flag.Parse()
 
@@ -46,15 +63,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	opts := ui.Options{
-		Providers:    providers,
-		ExtraColumns: extraColumns,
-		Verbose:      verbose,
-		Filter: ui.Filter{
-			Key:   filter.Key,
-			Value: filter.Value,
-		},
-	}
+	options.Providers = providers
 
 	var display ui.DisplayFunc
 	if interactiveMode {
@@ -63,7 +72,7 @@ func main() {
 		display = ui.Table
 	}
 
-	if err := display(ctx, opts); err != nil {
+	if err := display(ctx, options); err != nil {
 		cancel() // cleanup resources
 		fmt.Fprintln(os.Stderr, "displaying output:", err)
 		os.Exit(1)
