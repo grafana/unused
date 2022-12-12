5f4b5c712c81d2e3caf1af0abe1ca5007a0ca80f
Leandro López (inkel) <leandro.lopez@grafana.com>
2022-12-12 15:41:29 -0300

    Extend package documentation
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- disks.go
+++ disks.go
@@ -2,23 +2,28 @@ package unused
 
 import "sort"
 
-// Disks is a collection of Disk
+// Disks is a collection of Disk.
 type Disks []Disk
 
+// ByFunc is the type of sorting functions for Disks.
 type ByFunc func(p, q Disk) bool
 
+// ByProvider sorts a Disks collection by provider name.
 func ByProvider(p, q Disk) bool {
 	return p.Provider().Name() < q.Provider().Name()
 }
 
+// ByName sorts a Disks collection by disk name.
 func ByName(p, q Disk) bool {
 	return p.Name() < q.Name()
 }
 
+// ByCreatedAt sorts a Disks collection by disk creation time.
 func ByCreatedAt(p, q Disk) bool {
 	return p.CreatedAt().Before(q.CreatedAt())
 }
 
+// Sort sorts the collection by the given function.
 func (d Disks) Sort(by ByFunc) {
 	sort.Slice(d, func(i, j int) bool { return by(d[i], d[j]) })
 }
--- provider.go
+++ provider.go
@@ -2,17 +2,18 @@ package unused
 
 import "context"
 
-// Provider represents a cloud provider
+// Provider represents a cloud provider.
 type Provider interface {
-	// Name returns the provider name
+	// Name returns the provider name.
 	Name() string
 
-	// ListUnusedDisks returns a list of unused disks for the given provider
+	// ListUnusedDisks returns a list of unused disks for the given
+	// provider.
 	ListUnusedDisks(ctx context.Context) (Disks, error)
 
-	// Meta returns the provider metadata
+	// Meta returns the provider metadata.
 	Meta() Meta
 
-	// Delete deletes a disk from the provider
+	// Delete deletes a disk from the provider.
 	Delete(ctx context.Context, disk Disk) error
 }
