08469e84671a73d0bcee9e70e63200870cb54e2d
Leandro López (inkel) <leandro.lopez@grafana.com>
2023-09-13 10:11:54 -0300

    Add global navigation keys
    These will be used by other models to DRY the code.

--- cmd/unused/internal/ui/interactive/keys.go
+++ cmd/unused/internal/ui/interactive/keys.go
@@ -37,3 +37,15 @@ var keyMap = customKeyMap{
 	Toggle:   key.NewBinding(key.WithKeys(" "), key.WithHelp("<space>", "toggle mark")),
 	Delete:   key.NewBinding(key.WithKeys("x"), key.WithHelp("x", "delete marked")),
 }
+
+var navKeys = struct {
+	Quit, Up, Down, PageUp, PageDown, Home, End key.Binding
+}{
+	Quit:     key.NewBinding(key.WithKeys("q", "ctrl+c"), key.WithHelp("q", "quit")),
+	Up:       key.NewBinding(key.WithKeys("up"), key.WithHelp("↑", "up")),
+	Down:     key.NewBinding(key.WithKeys("down"), key.WithHelp("↓", "down")),
+	PageUp:   key.NewBinding(key.WithKeys("pgup", "right"), key.WithHelp("→", "page up")),
+	PageDown: key.NewBinding(key.WithKeys("pgdown", "left"), key.WithHelp("←", "page down")),
+	Home:     key.NewBinding(key.WithKeys("home"), key.WithHelp("home", "first")),
+	End:      key.NewBinding(key.WithKeys("end"), key.WithHelp("end", "last")),
+}
