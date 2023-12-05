7ae3000b126ff9e23776bab8463548e3d4afa81f
belén <rooneyshuman@gmail.com>
2023-12-05 09:56:45 -0800

    Collect duration in ms

--- cmd/unused-exporter/exporter.go
+++ cmd/unused-exporter/exporter.go
@@ -188,7 +188,7 @@ func (e *exporter) pollProvider(p unused.Provider) {
 			}
 
 			addMetric(e.info, 1)
-			addMetric(e.dur, dur.Microseconds())
+			addMetric(e.dur, dur.Milliseconds())
 			addMetric(e.suc, success)
 
 			for ns, di := range diskInfoByNamespace {
