f814d202ccd2c256cc525834ec999d258d4aaadf
Leandro López (inkel) <leandro.lopez@grafana.com>
2025-09-24 14:48:58 -0300

    Make listUnusedDisks a method of UI
    This simplifies also where we filter the disks to a single place, and
    will likely help to do the same for Interactive.

--- cmd/unused/internal/ui/table.go
+++ cmd/unused/internal/ui/table.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"sync"
 	"text/tabwriter"
 
-	"github.com/grafana/unused"
 	"github.com/grafana/unused/cmd/internal"
 )
 
@@ -19,19 +17,9 @@ var k8sHeaders = map[string]string{
 }
 
 func Table(ctx context.Context, ui UI) error {
-	disks, err := listUnusedDisks(ctx, ui.Providers)
+	disks, err := ui.listUnusedDisks(ctx)
 	if err != nil {
 		return err
-	}
-
-	if ui.Filter.Key != "" {
-		filtered := make(unused.Disks, 0, len(disks))
-		for _, d := range disks {
-			if d.Meta().Matches(ui.Filter.Key, ui.Filter.Value) {
-				filtered = append(filtered, d)
-			}
-		}
-		disks = filtered
 	}
 
 	if len(disks) == 0 {
@@ -89,46 +77,4 @@ func Table(ctx context.Context, ui UI) error {
 	}
 
 	return nil
-}
-
-func listUnusedDisks(ctx context.Context, providers []unused.Provider) (unused.Disks, error) {
-	var (
-		wg    sync.WaitGroup
-		mu    sync.Mutex
-		total unused.Disks
-	)
-
-	wg.Add(len(providers))
-
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	errCh := make(chan error, len(providers))
-
-	for _, p := range providers {
-		go func(p unused.Provider) {
-			defer wg.Done()
-
-			disks, err := p.ListUnusedDisks(ctx)
-			if err != nil {
-				cancel()
-				errCh <- fmt.Errorf("%s %s: %w", p.Name(), p.Meta(), err)
-				return
-			}
-
-			mu.Lock()
-			total = append(total, disks...)
-			mu.Unlock()
-		}(p)
-	}
-
-	wg.Wait()
-
-	select {
-	case err := <-errCh:
-		return nil, err
-	default:
-	}
-
-	return total, nil
 }
